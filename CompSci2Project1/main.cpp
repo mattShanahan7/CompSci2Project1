56bfdf00f0cf84e930900658de068e0dc83df6c5
Matthew <Matthew@Matts-MacBook-Pro-5.local>
2016-01-21 20:27:25 -0500

    Initial Commit

---
+++ CompSci2Project1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CompSci2Project1
+//
+//  Created by Matthew on 1/21/16.
+//  Copyright (c) 2016 Matthew. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
